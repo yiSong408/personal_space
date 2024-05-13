b928af65c6a0d2a9df830c77fd85ca6cd2dfe079
yiSong408 <songyi102788@gmail.com>
2024-05-13 21:26:45 +0100

    add index

---
+++ src/main/java/com/rongyi/personal_space/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.rongyi.personal_space.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
