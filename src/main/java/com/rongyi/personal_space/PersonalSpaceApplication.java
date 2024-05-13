41aedc95960c0934fa123a9cac6a3b151a7907a6
yiSong408 <songyi102788@gmail.com>
2024-05-13 20:53:41 +0100

    Initial commit

---
+++ src/main/java/com/rongyi/personal_space/PersonalSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.rongyi.personal_space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rongyi/personal_space/PersonalSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongyi.personal_space;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
